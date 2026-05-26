f82b5294700c65aab66ccbf63f4dd10ebafac062
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2026-05-26 17:22:55 +0200

    Update all dependencies and exclude bots from first-interaction (#41)
    * Update all dependencies to latest versions
    
    * Exclude bots from first-interaction job

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.46"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5"
+      version = "~> 3.9"
     }
   }
 }
